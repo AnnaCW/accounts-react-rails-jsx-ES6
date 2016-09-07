3c3b44b27c4105698df6b075ccc201cfb10862c0
Anna Weisbrodt <aweisbro@gmail.com>
2016-09-07 06:46:11 -0600

    add amount indicators

---
+++ app/assets/javascripts/components/amount_box.js.jsx
@@ -0,0 +1,19 @@
+'use strict';
+
+class AmountBox extends React.Component {
+  render() {
+    var panel = " panel panel-" + this.props.type;
+    return (
+      <div className='col-md-4'>
+        <div className={panel}>
+          <div className='panel-heading'>
+            {this.props.text}
+          </div>
+          <div className='panel-body'>
+            {amountFormat(this.props.amount)}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- app/assets/javascripts/components/records.js.jsx
+++ app/assets/javascripts/components/records.js.jsx
@@ -3,11 +3,33 @@
 class Records extends BaseComponent {
     constructor(props) {
         super();
-        this._bind('addRecord');
+        this._bind('addRecord', 'credits', 'debits', 'balance');
         this.state = {
             records: props.data
         };
     }
+
+  credits () {
+    return this.state.records.filter( (record) => {
+      return record.amount >= 0;
+    })
+    .reduce( (prev, curr) => {
+      return prev + parseFloat(curr.amount);
+    }, 0);
+  };
+
+  debits () {
+    return this.state.records.filter( (record) => {
+      return record.amount > 0;
+    })
+    .reduce( (prev, curr) => {
+      return prev + parseFloat(curr.amount);
+    }, 0);
+  };
+
+  balance() {
+    return this.debits() + this.credits()
+  }
 
   defaultProps () {
     return {records: [] }
@@ -15,7 +37,7 @@ class Records extends BaseComponent {
 
   addRecord(record) {
     var records = this.state.records;
-    records.push(record); 
+    records.push(record);
     this.setState({records: records});
   };
 
@@ -24,10 +46,18 @@ class Records extends BaseComponent {
       return <Record key={record.id} record={record} />
     })
     return (
-    <div className = 'records'>
-      <h2 className = 'title'>Records</h2>
-      <RecordForm handleNewRecord={this.addRecord} />
-      <table className = 'table table-bordered'>
+    <div className= 'records'>
+      <h2 className= 'title'>Records</h2>
+
+    <div className= 'row'>
+      <AmountBox type='success' amount={this.credits()} text='Credit'></AmountBox>
+      <AmountBox type='danger' amount={this.debits()} text='Debit'></AmountBox>
+      <AmountBox type='info' amount={this.balance()} text='Balance'></AmountBox>
+    </div>
+
+    <RecordForm handleNewRecord={this.addRecord} />
+    <hr />
+    <table className= 'table table-bordered'>
         <thead>
           <tr>
             <th>Date</th>
@@ -40,6 +70,7 @@ class Records extends BaseComponent {
       </tbody>
     </table>
   </div>
+
     )
   }
 
