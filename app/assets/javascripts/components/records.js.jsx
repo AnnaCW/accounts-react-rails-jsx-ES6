76c33055daa7c01ef18e878ccb9b06b072865889
Anna Weisbrodt <aweisbro@gmail.com>
2016-09-07 10:08:59 -0600

    react add-ons refactor

--- app/assets/javascripts/components/records.js.jsx
+++ app/assets/javascripts/components/records.js.jsx
@@ -10,9 +10,11 @@ class Records extends BaseComponent {
     }
 
   deleteRecord(record) {
-    var records = this.state.records.slice();
+    // var records = this.state.records.slice();
+    // var index = this.state.records.indexOf(record);
+    // records.splice(index, 1);
     var index = this.state.records.indexOf(record);
-    records.splice(index, 1);
+    var records = React.addons.update(this.state.records, {$splice: [[index, 1]] })
     this.setState({records: records});
   };
 
@@ -43,8 +45,7 @@ class Records extends BaseComponent {
   };
 
   addRecord(record) {
-    var records = this.state.records;
-    records.push(record);
+    var records = React.addons.update(this.state.records, {$push: [record] });
     this.setState({records: records});
   };
 
